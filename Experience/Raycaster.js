e4ab4da06537435368cd250fd2eb7b353c962c8f
pgpavlides <georgepavlideswork@gmail.com>
2022-10-23 13:33:07 +0300

    touch correction test

--- Experience/Raycaster.js
+++ Experience/Raycaster.js
@@ -38,10 +38,9 @@ export default class Raycaster extends EventEmitter {
   }
 
   onPointerClick() {
-    console.log(this.device)
-    if (this.device === "desktop") {
-      
-        window.addEventListener("click", () => {
+    console.log(this.device);
+    
+      window.addEventListener("pointerdown", () => {
         this.timeline = new GSAP.timeline();
 
         if (this.currentIntersect)
@@ -51,7 +50,7 @@ export default class Raycaster extends EventEmitter {
           // console.log(this.currentIntersect)
           if (this.object === "pyramid_button") {
             this.world.animations.firstStep();
-            // console.log("test")
+            console.log("test")
           }
 
           //--------Red Button
@@ -170,147 +169,10 @@ export default class Raycaster extends EventEmitter {
           if (this.object === "puzzle3_b9a") {
             this.world.puzzle3.p3_p9();
             this.world.puzzle3.lockCheck();
-          }          
+          }
         }
       });
-    } else if (this.device === "mobile") {
-        window.addEventListener("touchend", () => {
-            this.timeline = new GSAP.timeline();
     
-            if (this.currentIntersect)
-              console.log(this.currentIntersect)
-              this.object = this.currentIntersect.object.name;
-            {
-              // console.log(this.currentIntersect)
-              if (this.object === "pyramid_button") {
-                this.world.animations.firstStep();
-                // console.log("test")
-              }
-    
-              //--------Red Button
-              if (this.object === "Cube008") {
-                this.world.puzzle1.p1_Red();
-                this.world.puzzle1.lockCheck();
-                // console.log("test")
-              }
-              //--------Green Button
-              if (this.object === "Cube009") {
-                this.world.puzzle1.p1_Green();
-                this.world.puzzle1.lockCheck();
-              }
-              //--------Blue Button
-              if (this.object === "Cube010") {
-                this.world.puzzle1.p1_Blue();
-                this.world.puzzle1.lockCheck();
-              }
-              //--------Yellow Button
-              if (this.object === "Cube011") {
-                this.world.puzzle1.p1_Yellow();
-                this.world.puzzle1.lockCheck();
-              }
-    
-              //--------Phone 0
-              if (this.object === "Cube022_1") {
-                this.world.puzzle2.p2_p0();
-                this.world.puzzle2.lockCheck();
-              }
-              //--------Phone 9
-              if (this.object === "Cube021_1") {
-                this.world.puzzle2.p2_p9();
-                this.world.puzzle2.lockCheck();
-              }
-              //--------Phone 8
-              if (this.object === "Cube020_1") {
-                this.world.puzzle2.p2_p8();
-                this.world.puzzle2.lockCheck();
-              }
-              //--------Phone 7
-              if (this.object === "Cube019_1") {
-                this.world.puzzle2.p2_p7();
-                this.world.puzzle2.lockCheck();
-              }
-              //--------Phone 6
-              if (this.object === "Cube018_1") {
-                this.world.puzzle2.p2_p6();
-                this.world.puzzle2.lockCheck();
-              }
-              //--------Phone 5
-              if (this.object === "Cube017_1") {
-                this.world.puzzle2.p2_p5();
-                this.world.puzzle2.lockCheck();
-              }
-              //--------Phone 4
-              if (this.object === "Cube016_1") {
-                this.world.puzzle2.p2_p4();
-                this.world.puzzle2.lockCheck();
-              }
-              //--------Phone 3
-              if (this.object === "Cube015_1") {
-                this.world.puzzle2.p2_p3();
-                this.world.puzzle2.lockCheck();
-              }
-              //--------Phone 2
-              if (this.object === "Cube014_1") {
-                this.world.puzzle2.p2_p2();
-                this.world.puzzle2.lockCheck();
-              }
-              //--------Phone 1
-              if (this.object === "Cube013_1") {
-                this.world.puzzle2.p2_p1();
-                this.world.puzzle2.lockCheck();
-              }
-              //--------Puzzle_3_b1
-              if (this.object === "puzzle3_b1a") {
-                this.world.puzzle3.p3_p1();
-                this.world.puzzle3.lockCheck();
-              }
-              //--------Puzzle_3_b2
-              if (this.object === "puzzle3_b2a") {
-                this.world.puzzle3.p3_p2();
-                this.world.puzzle3.lockCheck();
-              }
-              //--------Puzzle_3_b3
-              if (this.object === "puzzle3_b3a") {
-                this.world.puzzle3.p3_p3();
-                this.world.puzzle3.lockCheck();
-              }
-              //--------Puzzle_3_b4
-              if (this.object === "puzzle3_b4a") {
-                this.world.puzzle3.p3_p4();
-                this.world.puzzle3.lockCheck();
-              }
-              //--------Puzzle_3_b4
-              if (this.object === "puzzle3_b5a") {
-                this.world.puzzle3.p3_p5();
-                this.world.puzzle3.lockCheck();
-              }
-              //--------Puzzle_3_b4
-              if (this.object === "puzzle3_b6a") {
-                this.world.puzzle3.p3_p6();
-                this.world.puzzle3.lockCheck();
-              }
-              //--------Puzzle_3_b7
-              if (this.object === "puzzle3_b7a") {
-                this.world.puzzle3.p3_p7();
-                this.world.puzzle3.lockCheck();
-              }
-              //--------Puzzle_3_b8
-              if (this.object === "puzzle3_b8a") {
-                this.world.puzzle3.p3_p8();
-                this.world.puzzle3.lockCheck();
-              }
-              //--------Puzzle_3_b9
-              if (this.object === "puzzle3_b9a") {
-                this.world.puzzle3.p3_p9();
-                this.world.puzzle3.lockCheck();
-              }          
-            }
-            
-          });
-        //   window.addEventListener("touchstart", () => {
-            
-        //   })
-    }
   }
 
   // onPointerHover(){
